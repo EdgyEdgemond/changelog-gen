ae1bc00b364cef78467dcc77e658ee2e9eac24f2
EdgyEdgemond <edgy.edgemond@gmail.com>
2020-01-23 10:41:02 +0000

    clean up

--- tests/test_version.py
+++ tests/test_version.py
@@ -10,8 +10,7 @@ class TestBumpVersion:
         monkeypatch.setattr(version.subprocess, "check_output", mock.Mock())
         version.subprocess.check_output.return_value = b"current_version=1.0.0\ncommit=False\nnew_version=1.1.0"
 
-        assert version.BumpVersion.get_version_info("patch") == {"current": "1.0.0", "new": "1.1.0"} 
-
+        assert version.BumpVersion.get_version_info("patch") == {"current": "1.0.0", "new": "1.1.0"}
 
     @pytest.mark.parametrize("semver", ["patch", "minor", "major"])
     def test_release(self, monkeypatch, semver):
